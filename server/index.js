ec964a697f6bf90bf9c002f6c6bb930dda7eb3df
FangmingCCC <60110690+FangmingCCC@users.noreply.github.com>
2020-11-05 21:58:28 +0800

    server

--- server/index.js
+++ server/index.js
@@ -8,24 +8,28 @@ if (existsSync("database.json")) {
     database = JSON.parse(readFileSync("database.json"));
 } else {
     database = {
-        wordScores: [],
-        gameScores: []
+        user: [],
+        searchResult: [],
+        courseDetail: []
     };
 }
 createServer(async (req, res) => {
     const parsed = parse(req.url, true);
 
-    if (parsed.pathname === '/wordScore') {
+    if (parsed.pathname === '/user') {
         let body = '';
         req.on('data', data => body += data);
         req.on('end', () => {
             const data = JSON.parse(body);
-            database.wordScores.push({
-                name: data.name,
-                word: data.word,
-                score: data.score
+            database.user.push({
+                userEmail: data.userEmail,
+                userPassword: data.userPassword,
+                userName: data.userName,
+                schoolName: data.schoolName,
+                gender: data.gender,
+                major: data.major
             });
-            console.log(database);
+            // console.log(database);
             
             writeFile("database.json", JSON.stringify(database), err => {
                 if (err) {
@@ -35,14 +39,19 @@ createServer(async (req, res) => {
                 }
             });
         });
-    } else if (parsed.pathname === '/gameScore') {
+    } else if (parsed.pathname === '/searchResult') {
         let body = '';
         req.on('data', data => body += data);
         req.on('end', () => {
             const data = JSON.parse(body);
-            database.gameScores.push({
-                name: data.name,
-                score: data.score
+            database.searchResult.push({
+                courseSchool: data.courseSchool,
+                subject: data.subject,
+                courseNumber: data.courseNumber,
+                courseProfessor: data,courseProfessor,
+                courseDifficulty: data.courseDifficulty,
+                courseTimeConsumption: data.courseTimeConsumption,
+                courseOverall: data.courseOverall
             });
             
             writeFile("database.json", JSON.stringify(database), err => {
@@ -53,15 +62,38 @@ createServer(async (req, res) => {
                 }
             });
         });
-    } else if (parsed.pathname === '/highestWordScores') {
-        res.end(JSON.stringify(
-            database.wordScores.sort((a, b) => b.score - a.score).filter((v, i) => i < 10)
-        ));
-    } else if (parsed.pathname === '/highestGameScores') {
-        res.end(JSON.stringify(
-            database.gameScores.sort((a, b) => b.score - a.score).filter((v, i) => i < 10)
-        ));
-    } else {
+    } else if (parsed.pathname === '/courseDetail') {
+        let body = '';
+        req.on('data', data => body += data);
+        req.on('end', () => {
+            const data = JSON.parse(body);
+            database.courseDetail.push({
+                studentName: data.studentName,
+                comment: data.comment,
+                courseDifficulty: data.courseDifficulty,
+                courseTimeConsumption: data.courseTimeConsumption,
+                courseOverall: data.courseOverall
+            });
+            
+            writeFile("database.json", JSON.stringify(database), err => {
+                if (err) {
+                    console.err(err);
+                }else{
+                    res.end();
+                }
+            });
+        });
+    } 
+    // else if (parsed.pathname === '/highestWordScores') {
+    //     res.end(JSON.stringify(
+    //         database.wordScores.sort((a, b) => b.score - a.score).filter((v, i) => i < 10)
+    //     ));
+    // } else if (parsed.pathname === '/highestGameScores') {
+    //     res.end(JSON.stringify(
+    //         database.gameScores.sort((a, b) => b.score - a.score).filter((v, i) => i < 10)
+    //     ));
+    // } 
+    else {
         // If the client did not request an API endpoint, we assume we need to fetch a file.
         // This is terrible security-wise, since we don't check the file requested is in the same directory.
         // This will do for our purposes.
