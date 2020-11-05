ad855173707c8d54fa10e1fe81d87e3fbce6fcb8
FangmingCCC <60110690+FangmingCCC@users.noreply.github.com>
2020-11-06 02:00:44 +0800

    new

---
+++ client/js/loadcreatecourse.js
@@ -0,0 +1,44 @@
+
+let coursename = '';
+document.getElementById('coursename').addEventListener('change',()=>{
+  coursename = document.getElementById('coursename').value;
+});
+
+let courseprofessor = '';
+document.getElementById('professor').addEventListener('change',()=>{
+  courseprofessor = document.getElementById('professor').value;
+});
+
+let subject = '';
+document.getElementById('majorCourse').addEventListener('change',()=>{
+  subject = document.getElementById('majorCourse').value;
+});
+
+let coursedifficulty = '';
+document.getElementById('easyLevel').addEventListener('change',()=>{
+  coursedifficulty = document.getElementById('easyLevel').value;
+});
+
+let coursetimeconsumption = '';
+document.getElementById('timeLevel').addEventListener('change',()=>{
+  coursetimeconsumption = document.getElementById('timeLevel').value;
+});
+
+let courseoverall = '';
+document.getElementById('overall').addEventListener('change',()=>{
+  courseoverall = document.getElementById('overall').value;
+});
+
+async function postCreateCourse(url = '',courseName,courseProfessor,subject, courseDifficulty, courseTimeConsumption, courseOverall, comment) {
+    await fetch(url, {
+      method: 'POST',  
+      headers: {
+        'Content-Type': "text/json"
+      }, 
+      body: JSON.stringify({ courseName : courseName, courseProfessor: courseProfessor,subject : subject, courseDifficulty: courseDifficulty, courseTimeConsumption: courseTimeConsumption, courseOverall: courseOverall, comment: comment })
+    });
+}
+
+document.getElementById('submit').addEventListener('click',()=>{
+    postCreateCourse('/createNewCourse', coursename, courseprofessor, subject, coursedifficulty, coursetimeconsumption, courseoverall);
+});
