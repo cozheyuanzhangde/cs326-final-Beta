bd54b4aead9d05146a671fd484e1ab068fe4c6b3
cozzyde <54963496+cozheyuanzhangde@users.noreply.github.com>
2020-11-14 12:53:08 +0800

    Update addnewcourse.js

--- client/js/addnewcourse.js
+++ client/js/addnewcourse.js
@@ -13,9 +13,9 @@ document.getElementById('coursenumber').addEventListener('change',()=>{
   coursenumber = document.getElementById('coursenumber').value;
 });
 
-let courseprofessor = '';
+let courseinstructor = '';
 document.getElementById('courseprofessor').addEventListener('change',()=>{
-  courseprofessor = document.getElementById('courseprofessor').value;
+  courseinstructor = document.getElementById('courseprofessor').value;
 });
 
 let coursedifficulty = '';
@@ -38,18 +38,29 @@ document.getElementById('coursecomment').addEventListener('change',()=>{
   coursecomment = document.getElementById('coursecomment').value;
 });
 
-async function postAddNewCourse(url = '', courseschoolname, coursesubject, coursenumber, courseprofessor, coursedifficulty, coursetime, courseoverall, coursecomment) {
+async function postAddNewCourse(url = '', courseschoolname, coursesubject, coursenumber, courseinstructor, coursedifficulty, coursetime, courseoverall) {
     await fetch(url, {
       method: 'POST',  
       headers: {
         'Content-Type': "text/json"
       }, 
-      body: JSON.stringify({ "courseschoolname": courseschoolname, "coursesubject": coursesubject, "coursenumber": coursenumber, "courseprofessor": courseprofessor, "coursedifficulty": coursedifficulty, "coursetime": coursetime, "courseoverall": courseoverall, "coursecomment": coursecomment })
+      body: JSON.stringify({ "schoolname": courseschoolname, "coursesubject": coursesubject, "coursenumber": coursenumber, "instructor": courseinstructor, "difficulty": coursedifficulty, "time": coursetime, "overall": courseoverall})
     });
 }
 
+async function postAddNewComment(url = '', courseid, username, coursecomment, coursedifficulty, coursetime, courseoverall) {
+  await fetch(url, {
+    method: 'POST',  
+    headers: {
+      'Content-Type': "text/json"
+    }, 
+    body: JSON.stringify({ "courseid": courseid, "username": username, "textcomment": coursecomment, "difficulty": coursedifficulty, "time": coursetime, "overall": courseoverall})
+  });
+}
+
 document.getElementById('submit').addEventListener('click',()=>{
-    postAddNewCourse('/addnewcourse', courseschoolname, coursesubject, coursenumber, courseprofessor, coursedifficulty, coursetime, courseoverall, coursecomment);
+    postAddNewCourse('/addnewcourse', courseschoolname, coursesubject, coursenumber, courseinstructor, coursedifficulty, coursetime, courseoverall, coursecomment);
+    postAddNewComment('/addnewcomment', courseid, username, coursecomment, coursedifficulty, coursetime, courseoverall);
     alert("Well Done! You successfully add a new course with a comment!");
     window.location.href = "./index.html";
 });
