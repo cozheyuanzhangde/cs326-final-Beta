b754bc35420e07366667f66c93b5e249dd7b8439
cozzyde <54963496+cozheyuanzhangde@users.noreply.github.com>
2020-11-15 11:39:39 +0800

    Update addnewcourse.js

--- client/js/addnewcourse.js
+++ client/js/addnewcourse.js
@@ -14,8 +14,8 @@ document.getElementById('coursenumber').addEventListener('change',()=>{
 });
 
 let courseinstructor = '';
-document.getElementById('courseprofessor').addEventListener('change',()=>{
-  courseinstructor = document.getElementById('courseprofessor').value;
+document.getElementById('courseinstructor').addEventListener('change',()=>{
+  courseinstructor = document.getElementById('courseinstructor').value;
 });
 
 let coursedifficulty = '';
@@ -38,30 +38,20 @@ document.getElementById('coursecomment').addEventListener('change',()=>{
   coursecomment = document.getElementById('coursecomment').value;
 });
 
-async function postAddNewCourse(url = '', courseschoolname, coursesubject, coursenumber, courseinstructor, coursedifficulty, coursetime, courseoverall) {
-    await fetch(url, {
-      method: 'POST',  
-      headers: {
-        'Content-Type': "text/json"
-      }, 
-      body: JSON.stringify({ "schoolname": courseschoolname, "coursesubject": coursesubject, "coursenumber": coursenumber, "instructor": courseinstructor, "difficulty": coursedifficulty, "time": coursetime, "overall": courseoverall})
-    });
-}
 
-async function postAddNewComment(url = '', courseid, username, coursecomment, coursedifficulty, coursetime, courseoverall) {
+async function postAddNewCourse(url = '', courseschoolname, coursesubject, coursenumber, courseinstructor, coursedifficulty, coursetime, courseoverall, username, textcomment) {
   await fetch(url, {
     method: 'POST',  
     headers: {
-      'Content-Type': "text/json"
+      'Content-Type': "application/json"
     }, 
-    body: JSON.stringify({ "courseid": courseid, "username": username, "textcomment": coursecomment, "difficulty": coursedifficulty, "time": coursetime, "overall": courseoverall})
+    body: JSON.stringify({ "schoolname": courseschoolname, "coursesubject": coursesubject, "coursenumber": coursenumber, "instructor": courseinstructor, "difficulty": coursedifficulty, "time": coursetime, "overall": courseoverall, "username": username, "textcomment": textcomment})
   });
 }
 
-document.getElementById('submit').addEventListener('click',()=>{
-    postAddNewCourse('/addnewcourse', courseschoolname, coursesubject, coursenumber, courseinstructor, coursedifficulty, coursetime, courseoverall, coursecomment);
-    
-    postAddNewComment('/addnewcomment', courseid, username, coursecomment, coursedifficulty, coursetime, courseoverall);
-    alert("Well Done! You successfully add a new course with a comment!");
-    window.location.href = "./index.html";
+document.getElementById('submit').addEventListener('click', async () => {
+  console.log(courseschoolname, coursesubject, coursenumber, courseinstructor, coursedifficulty, coursetime, courseoverall, 'Anonymous', coursecomment);
+  postAddNewCourse('/addnewcourse', courseschoolname, coursesubject, coursenumber, courseinstructor, coursedifficulty, coursetime, courseoverall, 'Anonymous', coursecomment);
+  alert("Well Done! You successfully add a new course with a comment!");
+  window.location.href = "./index.html";
 });
