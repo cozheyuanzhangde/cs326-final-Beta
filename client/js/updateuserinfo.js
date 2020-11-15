e82a963fade81554c8b60d51810cd61ff21a777b
cozzyde <54963496+cozheyuanzhangde@users.noreply.github.com>
2020-11-16 05:01:49 +0800

    Update updateuserinfo.js

--- client/js/updateuserinfo.js
+++ client/js/updateuserinfo.js
@@ -1,21 +1,30 @@
 let useremail = '';
 useremail = document.getElementById('useremail').value;
 
-let userpassword = '';
-userpassword = document.getElementById('userpassword').value;
+let userpassword = document.getElementById('userpasswordchanged').value;
+document.getElementById('userpasswordchanged').addEventListener('change',()=>{
+    userpassword = document.getElementById('userpasswordchanged').value;
+});
 
+let username = document.getElementById('username').value;
+document.getElementById('username').addEventListener('change',()=>{
+  username = document.getElementById('username').value;
+});
 
-let username = '';
-username = document.getElementById('username').value;
+let userschoolname = document.getElementById('userschoolname').value;
+document.getElementById('userschoolname').addEventListener('change',()=>{
+    userschoolname = document.getElementById('userschoolname').value;
+});
 
-let userschoolname = '';
-userschoolname = document.getElementById('userschoolname').value;
+let usergender = document.getElementById('usergender').value;
+document.getElementById('usergender').addEventListener('change',()=>{
+    usergender = document.getElementById('usergender').value;
+});
 
-let usergender = '';
-usergender = document.getElementById('usergender').value;
-
-let usermajor = '';
-usermajor = document.getElementById('usermajor').value;
+let usermajor = document.getElementById('usermajor').value;
+document.getElementById('usermajor').addEventListener('change',()=>{
+    usermajor = document.getElementById('usermajor').value;
+});
 
 const userid = 1; //For Testing Purpose
 
@@ -44,6 +53,8 @@ async function updateUserInfoNoPWDChange(url = '', userid, useremail, username, 
 document.getElementById('submit').addEventListener('click',()=>{
     if (userpassword === ""){
         updateUserInfoNoPWDChange('/updateuserinfonopwdchange', userid, useremail, username, userschoolname, usergender, usermajor);
+        alert("You have successfully changed your user profile!");
+        location.reload();
     }else{
         updateUserInfo('/updateuserinfo', userid, useremail, userpassword, username, userschoolname, usergender, usermajor);
     }
