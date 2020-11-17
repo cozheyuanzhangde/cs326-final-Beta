6bc8687953c1399744065d13910e88820cad0f11
jennyg1017 <jennyg1017@users.noreply.github.com>
2020-11-16 23:55:56 -0500

    Modules

--- server/index.js
+++ server/index.js
@@ -163,4 +163,174 @@ app.get("/loaduserinfobyuserid", async (req, res) => {
     res.send(JSON.stringify(user));
 });
 
-app.listen(process.env.PORT || 8080);
+//////////////////////////////////////////////////
+
+require('dotenv').config();
+
+const expressSession = require('express-session');  // for managing session state
+const passport = require('passport');               // handles authentication
+const LocalStrategy = require('passport-local').Strategy; // username/password strategy
+// const app = express();
+const port = process.env.PORT || 8080;
+
+// Session configuration
+
+const session = {
+    secret : process.env.SECRET || 'SECRET', // set this encryption key in Heroku config (never in GitHub)!
+    resave : false,
+    saveUninitialized: false
+};
+
+// Passport configuration
+
+const strategy = new LocalStrategy(
+    async (username, password, done) => {
+	if (!findUser(username)) {
+	    // no such user
+	    return done(null, false, { 'message' : 'Wrong username' });
+	}
+	if (!validatePassword(username, password)) {
+	    // invalid password
+	    // should disable logins after N messages
+	    // delay return to rate-limit brute-force attacks
+	    await new Promise((r) => setTimeout(r, 2000)); // two second delay
+	    return done(null, false, { 'message' : 'Wrong password' });
+	}
+	// success!
+	// should create a user object here, associated with a unique identifier
+	return done(null, username);
+    });
+
+// App configuration
+
+app.use(expressSession(session));
+passport.use(strategy);
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Convert user object to a unique identifier.
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+// Convert a unique identifier to a user object.
+passport.deserializeUser((uid, done) => {
+    done(null, uid);
+});
+
+app.use(express.json()); // allow JSON inputs
+app.use(express.urlencoded({'extended' : true})); // allow URLencoded data
+
+/////
+
+// we use an in-memory "database"; this isn't persistent but is easy
+let users = { 'emery' : 'compsci326' } // default user
+let userMap = {};
+
+// Returns true iff the user exists.
+function findUser(username) {
+    if (!users[username]) {
+	return false;
+    } else {
+	return true;
+    }
+}
+
+// Returns true iff the password is the one we have stored (in plaintext = bad but easy).
+function validatePassword(name, pwd) {
+    if (!findUser(name)) {
+	return false;
+    }
+    if (users[name] !== pwd) {
+	return false;
+    }
+    return true;
+}
+
+// Add a user to the "database".
+// Return true if added, false otherwise (because it was already there).
+// TODO
+function addUser(name, pwd) {
+	if(findUser(name) === false){
+		users[name] = pwd;
+		return true;
+	}
+	else{
+		return false;
+	}
+    // TODO
+}
+
+// Routes
+
+function checkLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+	// If we are authenticated, run the next route.
+	next();
+    } else {
+	// Otherwise, redirect to the login page.
+	res.redirect('/login');
+    }
+}
+
+app.get('/',
+	checkLoggedIn,
+	(req, res) => {
+	    res.send("hello world");
+	});
+
+// Handle post data from the login.html form.
+app.post('/login', 
+	 passport.authenticate('local' , {     // use username/password authentication
+	     'successRedirect' : '/index',   // when we login, go to /private 
+	     'failureRedirect' : '/login'      // otherwise, back to login
+	 }));
+
+// Handle the URL /login (just output the login.html file).
+app.get('/login',
+	(req, res) => res.sendFile('html/login.html',
+				   { 'root' : __dirname }));
+
+// Handle logging out (takes us back to the login page).
+app.get('/logout', (req, res) => {
+    req.logout(); // Logs us out!
+    res.redirect('/login'); // back to login
+});
+
+
+// Add a new user and password IFF one doesn't exist already.
+// If we successfully add a new user, go to /login, else, back to /register.
+// Use req.body to access data (as in, req.body['username']).
+// Use res.redirect to change URLs.
+// TODO
+app.post('/signup',
+	 (req, res) => {
+	     const username = req.body['username'];
+		 const password = req.body['password'];
+		 if(addUser(username,password) === true){
+			 res.redirect('/login');
+		 }
+		 else{
+			 res.redirect('/signup');
+		 }
+	     // TODO
+	     // Check if we successfully added the user.
+	     // If so, redirect to '/login'
+         // If not, redirect to '/register'.
+         if(addUser(username, password)){
+            res.redirect('/login');
+        } else {
+            res.redirect('/signup');
+        }
+	 });
+
+// Register URL
+app.get('/signup',
+	(req, res) => res.sendFile('html/signup.html',
+                   { 'root' : __dirname }));
+app.use(express.static('html'));
+
+app.get('*', (req, res) => {
+    res.send('Error');
+});       
+
+app.listen(port);
