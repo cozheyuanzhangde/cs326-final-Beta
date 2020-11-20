0023ac29292bbb2c3345a0d6d8f1cc2436690c53
cozzyde <54963496+cozheyuanzhangde@users.noreply.github.com>
2020-11-20 11:20:25 +0800

    Update indexpage.js

--- client/js/indexpage.js
+++ client/js/indexpage.js
@@ -1,3 +1,12 @@
-let currCookie;
-currCookie = document.cookie;
-console.log(currCookie);
+window.addEventListener("load", async function (){
+    const res_user = await fetch(`/getsession`,{
+        method: "GET"
+    });
+    if (!res_user.ok) {
+        console.log(res_user.error);
+        return;
+    }
+    const user = await res_user.json();
+
+    console.log(user.passport.user);
+});
