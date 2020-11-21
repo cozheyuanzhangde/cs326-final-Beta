963575f519ff9cc7113e97d3b3df9a4181ee0a77
cozzyde <54963496+cozheyuanzhangde@users.noreply.github.com>
2020-11-21 11:01:37 +0800

    Update userlogin.js

--- client/js/userlogin.js
+++ client/js/userlogin.js
@@ -1,38 +1,26 @@
+const alert = document.getElementById("epalert");
+const submitbtn = document.getElementById("submit");
+alert.style.visibility = "hidden";
+submitbtn.disabled = true;
+const emailInput = document.getElementById("useremail");
+const pwdInput = document.getElementById("userpassword");
 
-let useremail = '';
-document.getElementById('useremail').addEventListener('change',()=>{
-  useremail = document.getElementById('useremail').value;
-});
-
-let userpassword = '';
-document.getElementById('userpassword').addEventListener('change',()=>{
-    userpassword = document.getElementById('userpassword').value;
-});
-
-function focusMethodE() {           
-    document.getElementById("useremail").focus();
-}
-
-function focusMethodP() {           
-    document.getElementById("userpassword").focus();
-}
-
-
-/*document.getElementById('loginid').addEventListener('click', ()=>{
-    if(useremail === ''){
-        alert("Sorry, you should enter email, please try again!");
+emailInput.onkeyup = function() {
+    if ((emailInput.value.length > 0)&&(pwdInput.value.length > 0)){
+        alert.style.visibility = "hidden";
+        document.getElementById("submit").disabled = false;
+    }else{
+        alert.style.visibility = "visible";
+        document.getElementById("submit").disabled = true;
     }
-    else if(userpassword === ''){
-        alert("Sorry, you should enter password, please try again!");
+};
+
+pwdInput.onkeyup = function() {
+    if ((emailInput.value.length > 0)&&(pwdInput.value.length > 0)){
+        alert.style.visibility = "hidden";
+        submitbtn.disabled = false;
+    }else{
+        alert.style.visibility = "visible";
+        submitbtn.disabled = true;
     }
-    else if(!validateEmail(useremail)){
-        alert("You have entered an invalid email address! Please try again!");
-    }
-    else if(!validatePassword(userpassword)){
-        alert("You have entered an invalid password! Please try again!");
-    }
-    else if(validateEmail(useremail) && validatePassword(userpassword)){
-        alert("Login Success, jump back to main page.");
-        window.location.href = "./index.html";
-    }
-});*/
+};
