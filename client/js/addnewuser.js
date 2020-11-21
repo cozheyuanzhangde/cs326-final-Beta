013e85fe2daf446355b59abddc412c2236637556
cozzyde <54963496+cozheyuanzhangde@users.noreply.github.com>
2020-11-21 09:59:52 +0800

    Update addnewuser.js

--- client/js/addnewuser.js
+++ client/js/addnewuser.js
@@ -5,7 +5,14 @@ const letter = document.getElementById("letter");
 const number = document.getElementById("number");
 const length = document.getElementById("length");
 
-function focusMethod1() {           
+document.getElementById("emailalert").style.visibility = "hidden";
+document.getElementById("pwdalert").style.visibility = "hidden";
+document.getElementById("confpwdalert").style.visibility = "hidden";
+document.getElementById("submit").disabled = true;
+
+let valid1, valid2, valid3 = false;
+
+/*function focusMethod1() {           
   document.getElementById("useremail").focus();
 }
 
@@ -19,7 +26,7 @@ function focusMethod3() {
 
 userEmailInput.addEventListener(onclick, focusMethod1);
 userPwdInput.addEventListener(onclick, focusMethod2);
-userPwdConfInput.addEventListener(onclick, focusMethod3);
+userPwdConfInput.addEventListener(onclick, focusMethod3);*/
 
 // When the user clicks on the password field, show the message box
 userPwdInput.onfocus = function() {
@@ -31,27 +38,40 @@ userPwdInput.onblur = function() {
   document.getElementById("message").style.display = "none";
 };
 
-function checkEmail(){
-  const mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+userEmailInput.onkeyup = function() {
+  const mailformat = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
   if(userEmailInput.value.match(mailformat)){
-    userEmailInput.classList.remove("wrong");
+    userEmailInput.classList.remove("btn-outline-danger");
+    userEmailInput.classList.add("btn-outline-success");
+    document.getElementById("emailalert").style.visibility = "hidden";
+    valid1 = true;
   }else{
-    if(!userEmailInput.classList.contains("wrong")){
-      userEmailInput.classList.add("wrong");
-    }
+    userEmailInput.classList.add("btn-outline-danger");
+    userEmailInput.classList.remove("btn-outline-success");
+    document.getElementById("emailalert").style.visibility = "visible";
+    valid1 = false;
+  }
+  console.log(valid1,valid2,valid3);
+  if((valid1 === true)&&((valid2 === true))&&((valid3 === true))){
+    document.getElementById("submit").disabled = false;
+  }else{
+    document.getElementById("submit").disabled = true;
   }
 };
 
 // When the user starts to type something inside the password field
 userPwdInput.onkeyup = function() {
+  let check1,check2,check3 = false;
   // Validate letters
   const letters = /[a-zA-Z]/g;
   if(userPwdInput.value.match(letters)) {  
     letter.classList.remove("invalid");
     letter.classList.add("valid");
+    check1 = true;
   } else {
     letter.classList.remove("valid");
     letter.classList.add("invalid");
+    check1 = false;
   }
 
   // Validate numbers
@@ -59,33 +79,59 @@ userPwdInput.onkeyup = function() {
   if(userPwdInput.value.match(numbers)) {  
     number.classList.remove("invalid");
     number.classList.add("valid");
+    check2 = true;
   } else {
     number.classList.remove("valid");
     number.classList.add("invalid");
+    check2 = false;
   }
   
   // Validate length
   if(userPwdInput.value.length >= 6) {
     length.classList.remove("invalid");
     length.classList.add("valid");
+    check3 = true;
   } else {
     length.classList.remove("valid");
     length.classList.add("invalid");
+    check3 = false;
+  }
+  if((check1 === true)&&((check2 === true))&&((check3 === true))){
+    userPwdInput.classList.remove("btn-outline-danger");
+    userPwdInput.classList.add("btn-outline-success");
+    document.getElementById("pwdalert").style.visibility = "hidden";
+    valid2 = true;
+  }else{
+    userPwdInput.classList.add("btn-outline-danger");
+    userPwdInput.classList.remove("btn-outline-success");
+    document.getElementById("pwdalert").style.visibility = "visible";
+    valid2 = false;
+  }
+  console.log(valid1,valid2,valid3);
+  if((valid1 === true)&&((valid2 === true))&&((valid3 === true))){
+    document.getElementById("submit").disabled = false;
+  }else{
+    document.getElementById("submit").disabled = true;
   }
 };
 
 userPwdConfInput.onkeyup = function() {
-  if(userPwdConfInput.value.match(userPwdInput.value)){
-    userPwdConfInput.classList.remove("wrong");
+  if(userPwdConfInput.value === userPwdInput.value){
+    userPwdConfInput.classList.remove("btn-outline-danger");
+    userPwdConfInput.classList.add("btn-outline-success");
+    document.getElementById("confpwdalert").style.visibility = "hidden";
+    valid3 = true;
   }else{
-    if(!userPwdConfInput.classList.contains("wrong")){
-      userPwdConfInput.classList.add("wrong");
-    }
+    userPwdConfInput.classList.add("btn-outline-danger");
+    userPwdConfInput.classList.remove("btn-outline-success");
+    document.getElementById("confpwdalert").style.visibility = "visible";
+    valid3 = false;
+  }
+  console.log(valid1,valid2,valid3);
+  if((valid1 === true)&&((valid2 === true))&&((valid3 === true))){
+    document.getElementById("submit").disabled = false;
+  }else{
+    document.getElementById("submit").disabled = true;
   }
 };
-
-function validateEmail(email) {
-  const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(email);
-}
 
